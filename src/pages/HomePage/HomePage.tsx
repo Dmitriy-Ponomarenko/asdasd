3eb28c4ac98324d7c4abf003b500e4e7521c740a
Dmitriy-Ponomarenko <dponomar19@gmail.com>
2025-07-12 01:59:12 +0200

    initial commit

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,13 @@
+// src/pages/HomePage/HomePage.tsx
+
+import React from 'react';
+import styles from './HomePage.module.css';
+import '../../index.css';
+
+export const HomePage: React.FC = () => {
+  return (
+    <section className={`${styles.homeSection} section`}>
+      <div></div>
+    </section>
+  );
+};
